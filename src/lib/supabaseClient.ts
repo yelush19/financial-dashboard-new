9a14c94a9d22930e61967b0d0f42ab1affc3d7c0
Yelena  Enthin Adir <yelena@litay.co.il>
2025-10-15 11:59:22 +0300

    Fix supabase URL - correct domain

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://fcrmghhjdnnnvpzsqjeb.supabase.co'
+const supabaseUrl = 'https://fcrmghhjdnnnvpzsqieb.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjcm1naGhqZG5ubnZwenNxamViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg3NDQzNDgsImV4cCI6MjA0NDMyMDM0OH0.VT0gzqk8R7sJvSPH5eJIEK0RMPOAqNCIJW8ozhGPLxU'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   },
  define: {
   global: 'window',
-  'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://fcrmghhjdnnnvpzsqjeb.supabase.co'),
+  'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://fcrmghhjdnnnvpzsqieb.supabase.co'),
   'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjcm1naGhqZG5ubnZwenNxamViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg3NDQzNDgsImV4cCI6MjA0NDMyMDM0OH0.VT0gzqk8R7sJvSPH5eJIEK0RMPOAqNCIJW8ozhGPLxU')
 }
 })
