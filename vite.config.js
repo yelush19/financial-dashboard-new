59977053aacaf0d51b3d6fb91265378f3a38e16c
Yelena  Enthin Adir <yelena@litay.co.il>
2025-10-15 11:33:25 +0300

    Fix supabase client

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,9 @@ export default defineConfig({
       overlay: false
     }
   },
-  define: {
-    global: 'window'
-  }
+ define: {
+  global: 'window',
+  'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://fcrmghhjdnnnvpzsqjeb.supabase.co'),
+  'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjcm1naGhqZG5ubnZwenNxamViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg3NDQzNDgsImV4cCI6MjA0NDMyMDM0OH0.VT0gzqk8R7sJvSPH5eJIEK0RMPOAqNCIJW8ozhGPLxU')
+}
 })
