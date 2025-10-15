79e81524ec6af0eac1344b227f3c9933a7425670
Yelena  Enthin Adir <yelena@litay.co.il>
2025-10-15 11:27:09 +0300

    Hardcode supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://fcrmghhjdnnnvpzsqjeb.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjcm1naGhqZG5ubnZwenNxamViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg3NDQzNDgsImV4cCI6MjA0NDMyMDM0OH0.VT0gzqk8R7sJvSPH5eJIEK0RMPOAqNCIJW8ozhGPLxU'
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
